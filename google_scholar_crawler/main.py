fdcba4121d785a1321a45af1ee38ab32e948e176
bangbang <47984044+bbbdbbb@users.noreply.github.com>
2023-11-04 12:45:47 +0800

    Update main.py
    set citedby defualt 0

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -17,7 +17,7 @@ with open(f'results/gs_data.json', 'w') as outfile:
 shieldio_data = {
   "schemaVersion": 1,
   "label": "citations",
-  "message": f"{author['citedby']}",
+  "message": f"{0}",
 }
 with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
